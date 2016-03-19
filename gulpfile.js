8c5aa35354515668b00fa25b6126aca1f1af750d
codef0rmer <amit.2006.it@gmail.com>
2016-03-19 23:28:10 +0530

    add build step

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp   = require('gulp');
+var uglify = require('gulp-uglify');
+
+gulp.task('copy', function(cb) {
+  return gulp.src(['chrome-extension/manifest.json', 'chrome-extension/images/'])
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('uglify', function(cb) {
+  return gulp.src(['chrome-extension/js/*.js'])
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('dist', ['copy', 'uglify']);
